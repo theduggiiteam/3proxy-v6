39a21986e2b463637c77f0a6b79201f8eb7bee8a
The Duggii Team <theduggiiteam@gmail.com>
2025-03-24 01:52:51 +0700

    Update Blockadsnextdns.sh

--- Blockadsnextdns.sh
+++ Blockadsnextdns.sh
@@ -18,7 +18,7 @@ install_3proxy() {
     cd /3proxy
     URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.5.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.4
+    cd 3proxy-0.9.5
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     mv /3proxy/3proxy-0.9.5/bin/3proxy /usr/local/etc/3proxy/bin/
